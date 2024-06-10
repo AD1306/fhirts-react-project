e4fe5707e510edf8ae3184cf37872748756aa248
ad1306 <adave.dev@gmail.com>
2024-06-10 12:30:09 -0500

    beautify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { PatchUtils } from "@smile-cdr/fhirts";
 import React, { useState } from "react";
@@ -12,7 +11,7 @@ function App() {
     source: 0,
     value: {},
     valueDataType: "identifier",
-    insertIndex: 0
+    insertIndex: 0,
   });
   const [parameters, setParameters] = useState({
     resource: {},
@@ -27,47 +26,46 @@ function App() {
     // eslint-disable-next-line no-restricted-globals
     event.preventDefault();
     const patchUtils = new PatchUtils();
-    if(formData.operationType === "move") {
-      const moveParameters =  patchUtils
-      .createMoveParameters(
-        formData.path,
-        parseInt(formData.source),
-        parseInt(formData.destination)
-      )
-      .getPatchParameters();
+    if (formData.operationType === "move") {
+      const moveParameters = patchUtils
+        .createMoveParameters(
+          formData.path,
+          parseInt(formData.source),
+          parseInt(formData.destination)
+        )
+        .getPatchParameters();
       setParameters(() => ({ resource: moveParameters }));
-    } else if(formData.operationType === "delete") {
+    } else if (formData.operationType === "delete") {
       const deleteParameters = patchUtils
-      .createDeleteParameters(formData.path)
-      .getPatchParameters();
-      setParameters(() => ({ resource: deleteParameters}));
-    } else if(formData.operationType === "replace") {
+        .createDeleteParameters(formData.path)
+        .getPatchParameters();
+      setParameters(() => ({ resource: deleteParameters }));
+    } else if (formData.operationType === "replace") {
       const replaceParameters = patchUtils
-      .createReplaceParameters(
-        formData.path,
-        JSON.parse(formData.value),
-        getCorrespodingPatchDatatypeEnum(formData.valueDataType)
-      )
-      .getPatchParameters()
-      setParameters(() => ({resource:  replaceParameters}));
+        .createReplaceParameters(
+          formData.path,
+          JSON.parse(formData.value),
+          getCorrespodingPatchDatatypeEnum(formData.valueDataType)
+        )
+        .getPatchParameters();
+      setParameters(() => ({ resource: replaceParameters }));
     } else {
       const insertParameters = patchUtils
-      .createInsertParameters(
-        formData.path,
-        JSON.parse(formData.value),
-        getCorrespodingPatchDatatypeEnum(formData.valueDataType),
-        parseInt(formData.insertIndex)
-      )
-      .getPatchParameters()
-      setParameters(() => ({resource:  insertParameters}));
+        .createInsertParameters(
+          formData.path,
+          JSON.parse(formData.value),
+          getCorrespodingPatchDatatypeEnum(formData.valueDataType),
+          parseInt(formData.insertIndex)
+        )
+        .getPatchParameters();
+      setParameters(() => ({ resource: insertParameters }));
     }
-    
   }
 
   function getCorrespodingPatchDatatypeEnum(valueDataType) {
-    if(valueDataType === 'identifier') {
+    if (valueDataType === "identifier") {
       return PATCH_DATATYPE.IDENTIFIER;
-    } else if(valueDataType === 'address') {
+    } else if (valueDataType === "address") {
       return PATCH_DATATYPE.ADDRESS;
     }
     return PATCH_DATATYPE.HUMAN_NAME;
@@ -82,6 +80,7 @@ function App() {
           </div>
         </div>
       </nav>
+      <br />
       <div className="container">
         <div className="row">
           <h3 className="text-center">Patch Parameters creator</h3>
@@ -98,11 +97,10 @@ function App() {
                 value={formData.operationType}
                 onChange={handleChange}
               >
-                <option value="move" >Move</option>
+                <option value="move">Move</option>
                 <option value="delete">Delete</option>
                 <option value="replace">Replace</option>
                 <option value="insert">Insert</option>
-
               </select>
             </div>
             <div className="mb-3">
@@ -117,74 +115,79 @@ function App() {
                 onChange={handleChange}
               />
             </div>
-            {formData.operationType === "move" ? 
-            <div>
-            <div className="mb-3">
-              <label htmlFor="source" className="form-label">
-                Source
-              </label>
-              <input
-                name="source"
-                type="text"
-                className="form-control"
-                value={formData.source}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="destination" className="form-label">
-                Destination
-              </label>
-              <input
-                name="destination"
-                type="text"
-                className="form-control"
-                value={formData.destination}
-                onChange={handleChange}
-              />
-            </div> </div>: null}
+            {formData.operationType === "move" ? (
+              <div>
+                <div className="mb-3">
+                  <label htmlFor="source" className="form-label">
+                    Source
+                  </label>
+                  <input
+                    name="source"
+                    type="text"
+                    className="form-control"
+                    value={formData.source}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="destination" className="form-label">
+                    Destination
+                  </label>
+                  <input
+                    name="destination"
+                    type="text"
+                    className="form-control"
+                    value={formData.destination}
+                    onChange={handleChange}
+                  />
+                </div>{" "}
+              </div>
+            ) : null}
 
-            {formData.operationType === "replace" ||  formData.operationType === "insert"? 
-            <div>
-            <div className="mb-3">
-              <label htmlFor="value" className="form-label">
-                Value
-              </label>
-              <textarea
-                name="value"
-                type="text"
-                className="form-control"
-                value={formData.value}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="mb-3">
-            <select
-                name="valueDataType"
-                className="form-select"
-                value={formData.valueDataType}
-                onChange={handleChange}
-              >
-                <option value="identifier">Identifier</option>
-                <option value="address">Address</option>
-                <option value="humanName">Human Name</option>
-              </select>
-            </div> </div>: null}
+            {formData.operationType === "replace" ||
+            formData.operationType === "insert" ? (
+              <div>
+                <div className="mb-3">
+                  <label htmlFor="value" className="form-label">
+                    Value
+                  </label>
+                  <textarea
+                    name="value"
+                    type="text"
+                    className="form-control"
+                    value={formData.value}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <select
+                    name="valueDataType"
+                    className="form-select"
+                    value={formData.valueDataType}
+                    onChange={handleChange}
+                  >
+                    <option value="identifier">Identifier</option>
+                    <option value="address">Address</option>
+                    <option value="humanName">Human Name</option>
+                  </select>
+                </div>{" "}
+              </div>
+            ) : null}
 
-            {formData.operationType === "insert" ? 
-            <div className="mb-3">
-              <label htmlFor="insertIndex" className="form-label">
-                Insert Index
-              </label>
-              <input
-                name="insertIndex"
-                type="text"
-                className="form-control"
-                value={formData.insertIndex}
-                onChange={handleChange}
-              />
-             </div>: null}
-            
+            {formData.operationType === "insert" ? (
+              <div className="mb-3">
+                <label htmlFor="insertIndex" className="form-label">
+                  Insert Index
+                </label>
+                <input
+                  name="insertIndex"
+                  type="text"
+                  className="form-control"
+                  value={formData.insertIndex}
+                  onChange={handleChange}
+                />
+              </div>
+            ) : null}
 
             <button onClick={createPatchParameters} className="btn btn-primary">
               Create Parameters
@@ -192,9 +195,20 @@ function App() {
           </form>
         </div>
       </div>
-
+      <br />
       <div className="container">
-        <pre>{JSON.stringify(parameters.resource, undefined, 2)}</pre>
+        <div className="row">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Patch Parameters</h5>
+              <p class="card-text">
+                <div className="container">
+                  <pre>{JSON.stringify(parameters.resource, undefined, 2)}</pre>
+                </div>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
